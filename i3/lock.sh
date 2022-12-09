a5cfd92771001d06fc2f0ee7843f0327ff657938
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2022-12-09 22:48:45 +0100

    added i3wm configuration

---
+++ i3/lock.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+import -window root /tmp/screenshot.png
+#convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+convert /tmp/screenshot.png -spread 12 /tmp/screenshotblur.png
+#convert /tmp/screenshot.png -swirl 215 -spread 6 /tmp/screenshotblur.png
+rm /tmp/screenshot.png
+i3lock -i /tmp/screenshotblur.png
+
