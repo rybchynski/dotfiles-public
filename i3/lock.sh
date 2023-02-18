0e5b389415eb06bab50bfbfe3d44865bb52f2dea
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2023-02-18 20:02:48 +0100

    (refactor): update spread value for lock screen script

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 import -window root /tmp/screenshot.png
 #convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-convert /tmp/screenshot.png -spread 12 /tmp/screenshotblur.png
+convert /tmp/screenshot.png -spread 30 /tmp/screenshotblur.png
 #convert /tmp/screenshot.png -swirl 215 -spread 6 /tmp/screenshotblur.png
 rm /tmp/screenshot.png
 i3lock -i /tmp/screenshotblur.png
