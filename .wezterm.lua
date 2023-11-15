161ea293b2657df92d62e740135af18f22a50183
Yuriy Rybchynski <yuriyrybchynski.dev@gmail.com>
2023-11-15 17:18:26 +0200

    (feat): add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,56 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "ayu"
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 12
+
+-- Tabs:
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_opacity = 0.9
+
+-- Custom keymaps:
+config.keys = {
+	{
+		key = "i",
+		mods = "CTRL|ALT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "o",
+		mods = "CTRL|ALT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "R",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, _, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
